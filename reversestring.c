3fdac51cd6bf63c0b50f5fa612c69793d849cd4d
SABARNA6 <sabarnakrishbalu@gmail.com>
2024-01-15 14:25:52 +0530

    reversing string program stage1

---
+++ reversestring.c
@@ -0,0 +1,23 @@
+// this is reverse string
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{   int i;
+   char str[50],rev[50];
+   printf("Enter the string : ");
+   gets(str);
+for ( i=0;str[i]!='\0';i++);
+//printf("%d",i);
+i--;
+int j=0;
+while (i>=0)
+{
+
+rev[j]=str[i];
+j++;
+i--;
+}
+
+printf("%s",rev);
+}
